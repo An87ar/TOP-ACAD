2373b911b45df76cb59fb3ac7f53396296557a8a
An87ar <anar_salamov@bk.ru>
2024-05-18 20:25:24 +1000

    18/05/2024

--- Lesson_1/script.js
+++ Lesson_1/script.js
@@ -1,1 +1,56 @@
-alert( "Привет, мир!" );
+
+// Exercise 1
+let userName = prompt('как тебя зовут?');
+alert(`Привет ${userName}!`)
+
+// Exercise 2
+// const year = 2024;
+// let yearOfBirth = prompt('Укажите год рождения')
+// let age = year - yearOfBirth
+// alert(`Вам ${age} лет!`)
+
+// Exercise 3
+// let width = prompt('длинa стороны квадрата');
+// alert(`Периметр квадрата = ${4 * width}`)
+
+// Exercise 4
+// const pi = 3.14
+// let width = prompt('Укажите радиус окружности');
+// alert(`Площад окружности = ${pi * (width ** 2)}`)
+
+// Exercise 5
+// let distance = prompt('Укажите расстояние между городами в километрах');
+// let time = prompt('За какое время вы хотите проехать расстояние?')
+// let speed = distance / time
+// alert(`Необходимо двигаться со скоростью ${speed.toFixed(2)} км/час`)
+
+// Exercise 6
+// let dollar = prompt('сколько у вас долларов?')
+// const change = 0.92;
+// let euro = dollar * change;
+// alert(`у вас ${euro.toFixed(2)} евро`)
+
+// Exercise 7
+// let numb = prompt('Укажите обьем памяти флешкарты (в ГБ)') * 1000
+// alert(`во флешкарту поместится ${Math.floor(numb / 820)} файлов обьемом 820МБ`)
+
+// Exercise 8
+// let money = prompt('сколько у вас денег?')
+// let price = prompt('сколько стоит товар?')
+// let summ = Math.floor(money / price)
+// alert(`вы сможете купить ${summ} шт товара и у вас остаток денег ${money - (summ * price)}`)
+
+// Exercise 9
+// let numb = prompt('введдите число')
+// let numb1 = numb % 10
+// let numb2 = Math.floor((numb % 100) / 10)
+// let numb3 = Math.floor(numb / 100)
+// alert(`Перевернутое число: ${numb1}${numb2}${numb3}`)
+
+
+// Exercise 10
+// let numb = prompt('Введите число');
+// let numb2 = numb % 2;
+// (numb2 == 0) && alert('cetnoe cislo');
+// (numb2 != 0) && alert('ne cetnoe cislo');
+
